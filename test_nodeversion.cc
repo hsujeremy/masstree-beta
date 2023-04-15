4b29528fbe30e15fcc40174090dee041d7f60a6e
Jeremy Hsu <jeremyhsu1@gmail.com>
2023-04-14 23:29:54 -0400

    Explicitly initalize `nodeversion` object

--- test_nodeversion.cc
+++ test_nodeversion.cc
@@ -10,25 +10,27 @@ struct test_type : public Masstree::nodeparams<15, 15> {
 };
 
 void thread_try_lock(Masstree::node_base<test_type>::nodeversion_type *v) {
-    printf("Addr of nodeversion within thread %p\n", v);
-    if (v->try_lock()) (*v).unlock();
+    if (v->try_lock()) {
+        v->unlock();
+    }
 }
 
 int main() {
     printf("Starting nodeversion concurrency tests\n");
 
-    // TODO: assertion error hits with this line
-    Masstree::node_base<test_type>::nodeversion_type v;
+    Masstree::node_base<test_type>::nodeversion_type v(false);
+    // printf("v_ is %u\n", v.version_value());
 
-    printf("Test: single-threaded lock/unlock works... ");
+    printf("Test: single-threaded lock/unlock... ");
     v.try_lock();
     v.unlock();
     printf("Passed\n");
 
     // Make sure two threads cannot acquire the lock at the same time
-    printf("try lock with two threads\n");
+    printf("Test: two threads, concurrent lock/unlock... ");
     std::thread t1(thread_try_lock, &v);
     std::thread t2(thread_try_lock, &v);
     t1.join();
     t2.join();
+    printf("Passed\n");
 }
