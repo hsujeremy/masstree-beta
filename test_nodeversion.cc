169a6daee4eff9875ea9a31a3990d5892a6635b5
Jeremy Hsu <jeremyhsu1@gmail.com>
2023-04-17 11:07:59 -0400

    Add concurrent insert bit set/clear test

--- test_nodeversion.cc
+++ test_nodeversion.cc
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <iostream>
 #include <random>
 #include <thread>
@@ -22,6 +23,19 @@ void thread_try_lock(test_nodeversion* v) {
     }
 }
 
+void thread_mark_insert(test_nodeversion* v) {
+    if (!v->try_lock()) {
+        return;
+    }
+    assert(!v->inserting());
+    test_nodeversion initv = *v;
+    v->mark_insert();
+    assert(v->inserting());
+    v->assign_version(initv);
+    assert(!v->inserting());
+    v->unlock();
+}
+
 int main() {
     printf("Starting internal nodeversion concurrency tests...\n");
     test_nodeversion v(false);
@@ -31,19 +45,39 @@ int main() {
     v.unlock();
     v.lock();
     v.unlock();
-    printf("Single thread lock/unlock passed!\n");
+    printf("Single thread lock/unlock test passed!\n");
 
     // Test: multiple threads should not be able to obtain the lock concurrently
     srand(time(0));
-    for (int i = 0; i < 1000; ++i) {    
+    for (int i = 0; i < TRIALS; ++i) {    
         std::thread t1(thread_try_lock, &v);
         std::thread t2(thread_try_lock, &v);
         std::thread t3(thread_try_lock, &v);
+        std::thread t4(thread_try_lock, &v);
+        std::thread t5(thread_try_lock, &v);
         t1.join();
         t2.join();
         t3.join();
+        t4.join();
+        t5.join();
     }
-    printf("Threee threads concurrently locking/unlocking passed!\n");
+    printf("Concurrent locking/unlocking test passed!\n");
+
+    // Test: multiple threads shouldn't be able to set/clear inserting bit
+    // concurrently
+    for (int i = 0; i < TRIALS; ++i) {
+        std::thread t1(thread_mark_insert, &v);  
+        std::thread t2(thread_mark_insert, &v);
+        std::thread t3(thread_mark_insert, &v);
+        std::thread t4(thread_mark_insert, &v);
+        std::thread t5(thread_mark_insert, &v);
+        t1.join();
+        t2.join();
+        t3.join();
+        t4.join();
+        t5.join();
+    }
+    printf("Concurrent mark/clear insert bit test passed!\n");
 
     printf("All tests passed!\n");
 }
